2e1d33a31e60015a5d0e95c4e8e084d85e302718
Musa Turna <musat@MacBook-de-Musa.local>
2024-05-15 16:48:07 +0200

    Mise à jour du fichier

--- Membre.java
+++ Membre.java
@@ -1,3 +1,18 @@
-public class Membre {
+import java.util.Date;
 
+public class Membre extends Personne {
+    private String statut;
+    private Date dateAdhesion;
+
+    public Membre(String nom, String prénom, int id, Date dateAdhesion, String statut) {
+        super(nom, prénom, id);
+        this.statut = statut;
+        this.dateAdhesion = dateAdhesion;
+    }
+
+    @Override
+    public void afficherDetails() {
+        System.out.println("Nom: " + getNom() + ", Prénom: " + getPrénom() + ", ID: " + getId() +
+                           ", Statut: " + statut + ", Date d'adhésion: " + dateAdhesion);
+    }
 }
